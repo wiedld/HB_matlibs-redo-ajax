40b7e56ebff80c2bc6602be3a2a1e8c9eb1cfa04
Hackbright Student <info@hackbrightacademy.com>
2015-01-30 13:07:03 -0800

    accepts either POST or GET, and adapts request.*.get to accordingly.

--- madlibs.py
+++ madlibs.py
@@ -6,6 +6,11 @@ from flask import Flask, render_template, request
 # to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
+#  TRYING TO MAKE A GLOBAL VARIABLE, to use in routes.  NOT WORKING!!!
+#       use correct dictionary get method, based if request.method is "GET" or "POST".
+# dict_process = {"GET": request.args.get, "POST": request.form.get}
+
+
 # route to handle the landing page of a website.
 @app.route('/')
 def start_here():
@@ -13,38 +18,47 @@ def start_here():
     # return "Hi! This is the home page."
     return render_template("index.html")
 
+
 # route to display a simple web page
 @app.route('/hello')
 def say_hello():
     print "running say_hello"
     return render_template("hello.html")
 
-@app.route('/greet')
+
+@app.route('/greet', methods=["GET","POST"])
 def greet_person():
     print "running greet_person"
-    player = request.args.get("person")
+    #use correct dictionary get method, based if request.method is "GET" or "POST".
+    dict_process = {"GET": request.args.get, "POST": request.form.get}
+    player = dict_process[request.method]("person")
     AWESOMENESS = [
         'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
         'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
     compliment = choice(AWESOMENESS)
     return render_template("game_question.html", person=player, compliment=compliment)
 
-@app.route('/gameURL')
+
+@app.route('/gameURL', methods=["GET","POST"])
 def show_game_question():
     print "show_game_question running"
-    if request.args.get("play_yes") == "True":
+    dict_process = {"GET": request.args.get, "POST": request.form.get}
+    if (dict_process[request.method])("play_yes") == "True":
         return render_template("game.html")
     else:
         return render_template("goodbye.html")
 
-@app.route('/madlib')
+
+@app.route('/madlib', methods=["GET","POST"])
 def show_madlibs():
     print "show_game_form running"
-    name = request.args.get("person")
-    col = request.args.get("favcolor")
-    adj = request.args.get("adjective")
-    noun = request.args.get("noun")
-    num = request.args.get("number")
+    # use correct dictionary get method, based if request.method is "GET" or "POST".
+    dict_process = {"GET": request.args.get, "POST": request.form.get}
+    name = dict_process [request.method] ("person")
+    col = dict_process [request.method] ("favcolor")
+    adj = dict_process [request.method] ("adjective")
+    noun = dict_process [request.method] ("noun")
+    num = dict_process [request.method] ("number")
     IDnum = str( randrange(1,4) )
     print IDnum
     print type(IDnum)
