17e48ea270fdd57e061500ba8c516f02a731dbad
Hackbright Student <info@hackbrightacademy.com>
2015-02-16 14:50:15 -0800

    Interactive game, all on 1 template, with jquery/ajax for all steps.

--- madlibs.py
+++ madlibs.py
@@ -1,21 +1,20 @@
 from random import choice, randrange
 from flask import Flask, render_template, request, session, jsonify
+import json
 
 
-# "__name__" is a special Python variable for the name of the current module; Flask wants
-# to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
 
 
-# route to handle the landing page of a website.
+# Starting index.html asks for the user's name.
 @app.route('/')
 def start_here():
-    # return "Hi! This is the home page."
     return render_template("index.html")
 
 
-
+# Greets the user by name, assign an AWESOME factor. 
+# Ask if want to play game.
 @app.route('/greet')
 def greet_person():
     name = request.args.get("name")
@@ -27,40 +26,47 @@ def greet_person():
     return greeting
 
 
+# based on user response, gives either goodbye or AJAX in the game.
 @app.route('/gameURL', methods=['POST'])
 def show_game_question():
     answer = request.form.get("play_yes")
-    print answer
     if answer == "True":
-        game_or_goodbye = "play game!"
+        file_obj = open("./static/game.txt")
+        game_list=[]
+        for row in file_obj.readlines():
+            game_list.append(row.rstrip("\n"))
+        game_string = "</br>".join(game_list)
+        game_or_goodbye = game_string
     else:
         game_or_goodbye = '<p>Sorry to see you go :( <br/> Come back and play soon, k?</p>'
     return game_or_goodbye
 
 
-@app.route('/madlib', methods=["GET","POST"])
+# Takes the posted data (serialized in js file), places in dict 
+#  and returns with Madlib text + a button to refresh with new dict.
+@app.route('/madlib', methods=['POST'])
 def show_madlibs():
-    print "show_game_form running"
-    # use correct dictionary get method, based if request.method is "GET" or "POST".
-    dict_process = {"GET": request.args.get, "POST": request.form.get}
-    name = dict_process [request.method] ("person")
-    col = dict_process [request.method] ("favcolor")
-    adj = dict_process [request.method] ("adjective")
-    noun = dict_process [request.method] ("noun")
-    num = dict_process [request.method] ("number")
-    IDnum = str( randrange(1,4) )
-    print IDnum
-    print type(IDnum)
-    return render_template("madlibs.html", 
-        MLname=name, 
-        MLcolor=col, 
-        MLadjective=adj, 
-        MLnoun=noun,
-        MLnum=num,
-        MLtemplateID = IDnum)
+    name = request.form.get("person")
+    col = request.form.get("favcolor")
+    adj = request.form.get("adjective")
+    noun = request.form.get("noun")
+    num = request.form.get("number")
+    print ("this should be the color:",col)
+
+    madlib_dict = {
+        "MLname": name,
+        "MLcolor": col, 
+        "MLadjective": adj, 
+        "MLnoun": noun,
+        "MLnum": num
+        }
+    
+    madlib = "<p>There once was a l33t programmer named "+ madlib_dict['MLname'] +",<br /> who had "+ madlib_dict['MLnum'] +" cats and a dog. <br /> Although many found her "+ madlib_dict['MLadjective'] +", she was <br /> better described as the "+ madlib_dict['MLnoun'] +". <br /> Fortunately, this was not a problem.  Because the <br /> instructor, who had a "+ madlib_dict['MLcolor'] +" nose, was <br /> even more extremely "+ madlib_dict['MLadjective'] +".</p>"
+
+    return madlib
+
 
 
 if __name__ == '__main__':
-    # debug=True gives us error messages in the browser and also "reloads" our web app
-    # if we change the code.
     app.run(debug=True)
+
--- static/madlibs-scripts.js
+++ static/madlibs-scripts.js
@@ -1,5 +1,5 @@
 
-// Script to replace all the html in the form #msg-to-user, after Step #1.
+// Script to replace all the html in the form #content_form, after Step #1.
     //Takes the name, and returns a greeting and asks to launch game.
 
 function greetByName(evt){
@@ -16,7 +16,7 @@ $("#hello").on('click',greetByName);
 
 
 
-// Script to replace all the html in the form #msg-to-user, after Step #2.
+// Script to replace all the html in the form #content_form, after Step #2.
     // Takes the user preference (to play or not to play), and then routes in 2 possible directions.
     // If gaming a game, will return the entire html text for the game in a string from the flask controller.
 
@@ -26,16 +26,48 @@ function toGameOrNot(evt){
     // looks at both radio buttons, and filters by checked
     var user_preference = (jQuery("input[name=play_yes]")).filter(":checked");
     $.post("/gameURL", user_preference,function(result){
-        console.log(result);
         $("#current-task").html(result);
     });
     $("#game").hide();
+    $("#madlibs_submit").show();
+    $("#clear").show();
 }
 
 $("#game").on("click", toGameOrNot);
 
 
 
-//TODO:  Script to replace all the html in the form #msg-to-user, after Step #3.
+// Script to replace all the html in the form #content_form, after Step #3.
     //  take the inputs from the game, and return the complete Madlibs into the div #madlib.
 
+function madlibs_submit(evt){
+    evt.preventDefault();
+    console.log("#3 is running");
+    // get form values & serialize, send to flask, get returned Matlab.
+    var data = $("#content_form").serialize();
+    $.post("/madlib", data, function(result){
+        $("#madlib").html(result);
+    });
+    $("#re_do").show();
+}
+
+$("#madlibs_submit").on("click", madlibs_submit);
+
+
+
+// Script to play again, with new mablibs input.
+
+function madlibs_again(evt){
+    evt.preventDefault();
+    console.log("#4 is running");
+    $("#madlib").empty();
+    // Hide the re-do button.  Initiate game form.
+    // var user_preference = (jQuery("input[name=play_yes]")).value("True");
+    $.post("/gameURL", {"play_yes":"True"},function(result){
+        $("#current-task").html(result);
+    });
+    $("#re_do").hide();
+
+}
+
+$("#re_do").on("click", madlibs_again);
